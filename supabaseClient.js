f6d63b575f6a5e737fdfe3ca55392206f6ce7765
vishhsrblr <vishhsrblr@gmail.com>
2025-01-02 08:14:53 +0100

    Initial commit

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://sqclgvuwzhcqwpkeggav.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxY2xndnV3emhjcXdwa2VnZ2F2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3NjM5MTYsImV4cCI6MjA1MTMzOTkxNn0.onPS8pjY1QoH_xcAoXA39nG8vKjzPkB_j6vvpgIC_lE'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Missing Supabase environment variables')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
